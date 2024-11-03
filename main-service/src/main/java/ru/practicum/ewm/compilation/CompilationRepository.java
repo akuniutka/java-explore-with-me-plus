c7147a7fbddc7b8552b5411b6b0c7072d7432709
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2024-11-03 17:45:06 +0300

    Fix: Implement method save() in CompilationService

---
+++ main-service/src/main/java/ru/practicum/ewm/compilation/CompilationRepository.java
@@ -0,0 +1,6 @@
+package ru.practicum.ewm.compilation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface CompilationRepository extends JpaRepository<Compilation, Long> {
+}
---
+++ main-service/src/main/java/ru/practicum/ewm/compilation/CompilationServiceImpl.java
@@ -0,0 +1,24 @@
+package ru.practicum.ewm.compilation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.practicum.ewm.event.Event;
+import ru.practicum.ewm.event.EventService;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CompilationServiceImpl implements CompilationService {
+    private final EventService eventService;
+    private final CompilationMapper mapper;
+    private final CompilationRepository repository;
+
+    @Override
+    public CompilationDto save(final NewCompilationDto requestDto) {
+        final List<Long> eventIds = requestDto.events();
+        final List<Event> relatedEvents = eventService.getByIds(eventIds);
+        final Compilation compilation = mapper.mapToCompilation(requestDto, relatedEvents);
+        return mapper.mapToDto(repository.save(compilation));
+    }
+}
