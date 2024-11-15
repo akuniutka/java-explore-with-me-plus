d30c408452d8f6f1fc20cb47dbe1bf8e5555261b
Andrei Kuniutka <akuniutka@gmail.com>
2024-11-15 22:42:36 +0300

    Fix: Add autotests for NotFoundException handling

--- main-service/src/main/java/ru/practicum/ewm/exception/NotFoundException.java
+++ main-service/src/main/java/ru/practicum/ewm/exception/NotFoundException.java
@@ -2,7 +2,10 @@ package ru.practicum.ewm.exception;
 
 import lombok.Getter;
 
+import java.util.LinkedHashSet;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 public class NotFoundException extends RuntimeException {
@@ -11,9 +14,7 @@ public class NotFoundException extends RuntimeException {
     private final Set<Long> modelIds;
 
     public NotFoundException(final String modelName, final Long modelId) {
-        super("%s with id = %s not found".formatted(modelName, modelId));
-        this.modelName = modelName;
-        this.modelIds = Set.of(modelId);
+        this(modelName, Set.of(modelId));
     }
 
     public <T> NotFoundException(final Class<T> modelClass, final Long modelId) {
@@ -21,12 +22,26 @@ public class NotFoundException extends RuntimeException {
     }
 
     public NotFoundException(final String modelName, final Set<Long> modelIds) {
-        super("%s with ids = %s not found".formatted(modelName, modelIds));
+        Objects.requireNonNull(modelIds);
         this.modelName = modelName;
-        this.modelIds = modelIds;
+        this.modelIds = modelIds.stream()
+                .sorted()
+                .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 
     public <T> NotFoundException(final Class<T> modelClass, final Set<Long> modelIds) {
         this(modelClass.getSimpleName(), modelIds);
+    }
+
+    @Override
+    public String getMessage() {
+        if (modelIds.size() == 1) {
+            return "%s with id = %s not found".formatted(modelName, modelIds.iterator().next());
+        }
+        final String modelIdsStr = modelIds.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
+        final String modelNamePlural = "Category".equals(modelName) ? "Categories" : modelName + "s";
+        return "%s with id = %s not found".formatted(modelNamePlural, modelIdsStr);
     }
 }
