1a99ec1de7ba729730b43f3beae9c84a5a8e0cab
Andrei Kuniutka <akuniutka@gmail.com>
2024-11-29 21:43:59 +0300

    Feat: Log requests to main service to Elasticsearch + Kibana

--- main-service/src/main/java/ru/practicum/ewm/MainService.java
+++ main-service/src/main/java/ru/practicum/ewm/MainService.java
@@ -3,6 +3,10 @@ package ru.practicum.ewm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.zalando.logbook.HttpLogFormatter;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.json.JsonHttpLogFormatter;
+import org.zalando.logbook.logstash.LogstashLogbackSink;
 
 import java.time.Clock;
 
@@ -16,5 +20,14 @@ public class MainService {
     @Bean
     Clock clock() {
         return Clock.systemDefaultZone();
+    }
+
+    @Bean
+    public Logbook logbook() {
+        final HttpLogFormatter formatter = new JsonHttpLogFormatter();
+        final LogstashLogbackSink logstashsink = new LogstashLogbackSink(formatter);
+        return Logbook.builder()
+                .sink(logstashsink)
+                .build();
     }
 }
