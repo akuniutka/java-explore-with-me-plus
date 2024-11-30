8144a27527424fb3274c441024f8659bb0525ef7
Andrei Kuniutka <akuniutka@gmail.com>
2024-11-30 20:56:35 +0300

    Fix: Exclude requests to /actuator/health from logging

--- main-service/src/main/java/ru/practicum/ewm/configuration/MainServiceConfig.java
+++ main-service/src/main/java/ru/practicum/ewm/configuration/MainServiceConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.logbook.HttpLogFormatter;
 import org.zalando.logbook.Logbook;
+import org.zalando.logbook.core.Conditions;
 import org.zalando.logbook.json.JsonHttpLogFormatter;
 import org.zalando.logbook.logstash.LogstashLogbackSink;
 
@@ -20,9 +21,10 @@ public class MainServiceConfig {
     @Bean
     public Logbook logbook() {
         final HttpLogFormatter formatter = new JsonHttpLogFormatter();
-        final LogstashLogbackSink logstashsink = new LogstashLogbackSink(formatter);
+        final LogstashLogbackSink logstashSink = new LogstashLogbackSink(formatter);
         return Logbook.builder()
-                .sink(logstashsink)
+                .condition(Conditions.exclude(Conditions.requestTo("/actuator/health")))
+                .sink(logstashSink)
                 .build();
     }
 }
