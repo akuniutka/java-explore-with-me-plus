fab97a381baaa3779f79e37ba2b7ac06c9cf9e42
Andrei Kuniutka <akuniutka@gmail.com>
2024-12-01 12:29:22 +0300

    Fix: Log requests to stats service to Elasticsearch + Kibana

--- stats/stats-service/src/main/java/ru/practicum/ewm/configuration/StatsServiceConfig.java
+++ stats/stats-service/src/main/java/ru/practicum/ewm/configuration/StatsServiceConfig.java
@@ -2,6 +2,11 @@ package ru.practicum.ewm.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.HttpLogFormatter;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.core.Conditions;
+import org.zalando.logbook.json.JsonHttpLogFormatter;
+import org.zalando.logbook.logstash.LogstashLogbackSink;
 
 import java.time.Clock;
 
@@ -11,5 +16,15 @@ public class StatsServiceConfig {
     @Bean
     Clock clock() {
         return Clock.systemDefaultZone();
+    }
+
+    @Bean
+    public Logbook logbook() {
+        final HttpLogFormatter formatter = new JsonHttpLogFormatter();
+        final LogstashLogbackSink logstashSink = new LogstashLogbackSink(formatter);
+        return Logbook.builder()
+                .condition(Conditions.exclude(Conditions.requestTo("/actuator/health")))
+                .sink(logstashSink)
+                .build();
     }
 }
